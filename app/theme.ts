e7d5646106a0c910a39e9e8005d5df738f560128
Sean Belke <belkesean@gmail.com>
2026-02-25 22:08:15 -0500

    Revert "Apply Cerulean/Peach/Sea Green/Carbon/Wine palette consistently"
    This reverts commit bbd2771c34e0d19b89103e29d91ad919590a3c92.

--- app/theme.ts
+++ app/theme.ts
@@ -4,32 +4,13 @@ const theme = createTheme({
   palette: {
     mode: "light",
     primary: {
-      main: "#008148",
-      dark: "#00663a",
-      light: "#39a96b",
-      contrastText: "#ffffff",
+      main: "#0f766e",
     },
     secondary: {
-      main: "#2D728F",
-      dark: "#255e75",
-      light: "#5793ab",
-      contrastText: "#ffffff",
-    },
-    error: {
-      main: "#632B30",
-      contrastText: "#ffffff",
-    },
-    warning: {
-      main: "#FFCDB2",
-      dark: "#7f4e35",
-      contrastText: "#1C1D21",
-    },
-    text: {
-      primary: "#1C1D21",
-      secondary: "#3f454e",
+      main: "#334155",
     },
     background: {
-      default: "#f2f6f7",
+      default: "#f7f8fb",
       paper: "#ffffff",
     },
   },
